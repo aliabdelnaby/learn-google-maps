ce40bee4247546a1968600f1ab2f31760a417cae
aliabdelnaby <aliabdelnabi239@gmail.com>
2024-06-22 23:56:56 +0300

    Camera Bounds

--- lib/widgets/custom_google_map.dart
+++ lib/widgets/custom_google_map.dart
@@ -22,9 +22,9 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
       //! Zoom Levels
       // world view 0 -> 3
       // country view 4 -> 6
-      // world view 10 -> 12
-      // world view 13 -> 17
-      // world view 18 -> 20
+      // city view 10 -> 12
+      // street view 13 -> 17
+      // building view 18 -> 20
     );
     super.initState();
   }
@@ -33,6 +33,18 @@ class _CustomGoogleMapState extends State<CustomGoogleMap> {
   Widget build(BuildContext context) {
     return GoogleMap(
       initialCameraPosition: initialCameraPosition,
+      cameraTargetBounds: CameraTargetBounds(
+        LatLngBounds(
+          southwest: const LatLng(
+            30.03737748732558,
+            31.23417640063827,
+          ),
+          northeast: const LatLng(
+            30.08967415776431,
+            31.260918861245514,
+          ),
+        ),
+      ),
     );
   }
 }
